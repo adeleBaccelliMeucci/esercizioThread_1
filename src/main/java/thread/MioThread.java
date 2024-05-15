221666abbdf104030e69192081224bd5f3e804c6
Adele <baccelli.adele@itismeucci.com>
2024-05-16 00:45:59 +0200

    Finito

--- src/main/java/thread/MioThread.java
+++ src/main/java/thread/MioThread.java
@@ -3,9 +3,10 @@ package thread;
 public class MioThread extends Thread{
 
     public MioThread(){
-        
+
     }
 
+    
     @Override
     public void run() {
         for(int i = 0; i < 10; i++){
